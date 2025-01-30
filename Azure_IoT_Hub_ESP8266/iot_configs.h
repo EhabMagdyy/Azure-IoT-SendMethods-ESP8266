bce584f6a2d51ee474f1a07b4d482dd3a4c46cb5
EhabMagdyy <ehabmagdy0044@gmail.com>
2025-01-31 00:48:00 +0200

    first commit

---
+++ Azure_IoT_Hub_ESP8266/iot_configs.h
@@ -0,0 +1,14 @@
+/********************************************************************************************************************
+*****************************    Author  : Ehab Magdy Abdullah                      *********************************
+*****************************    Linkedin: https://www.linkedin.com/in/ehabmagdyy/  *********************************
+*****************************    Youtube : https://www.youtube.com/@EhabMagdyy      *********************************
+*********************************************************************************************************************/
+
+// Wifi
+#define IOT_CONFIG_WIFI_SSID      "2001"
+#define IOT_CONFIG_WIFI_PASSWORD  "19821968"
+
+// Azure IoT
+#define IOT_CONFIG_IOTHUB_FQDN    "ehabhub.azure-devices.net"
+#define IOT_CONFIG_DEVICE_ID      "ehabdev"
+#define IOT_CONFIG_DEVICE_KEY     "mHTh4VFmxGpPHXKuI2TGCDsPezFK53gTFMloOd5PB1Y="
